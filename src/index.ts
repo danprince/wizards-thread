f80c9162f2834a637fa43b2513a6b99ca2cf8b7b
danprince <northwalesdan@gmail.com>
2020-10-08 14:49:15 +0100

    adds all cards to starting deck

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Game } from "./game";
-import { Remedy, Bolt, Augment, Burst, Hypothesis, Propel, Antithesis, Synthesis, Justify, Mystify, Purify, Amplify, Siphon, Again, Supernova, Salvation, Awaken, Obstacle, Fatigue, Confused, Wormhole, Loop, End } from "./cards";
+import * as Cards from "./cards";
 import { Scrabbler } from "./monsters";
 import { init } from "./ui";
 import { StartTurnAction } from "./actions";
@@ -8,31 +8,11 @@ let game = new Game();
 
 game.start();
 
-game.deck = [
-  new Remedy,
-  new Bolt,
-  new Augment,
-  new Burst,
-  new Propel,
-  new Hypothesis,
-  new Antithesis,
-  new Synthesis,
-  new Justify,
-  new Mystify,
-  new Purify,
-  new Amplify,
-  new Siphon,
-  new Again,
-  new Supernova,
-  new Salvation,
-  new Awaken,
-  new Obstacle,
-  new Fatigue,
-  new Confused,
-  new Wormhole,
-  new Loop,
-  new End,
-];
+game.deck = [];
+
+for (let Card of Object.values(Cards)) {
+  game.deck.push(new Card);
+}
 
 game.monster = new Scrabbler();
 
